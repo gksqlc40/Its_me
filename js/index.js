e34cfcbf50d682f5bfa9f59d152669d7dc0cfc37
gksqlc40 <gksqlc40@gmail.com>
2020-06-26 16:14:20 +0900

    0625

--- js/index.js
+++
@@ -1,9 +0,0 @@
-var indexMyFavorite = document.getElementsByClassName("myFavorite");
-var Onclick = document.getElementsByClassName("myFavorite").onclick;
-function doDisplay() {
-  if (indexMyFavorite.style.display === "block") {
-    indexMyFavorite.style.display = "none";
-  } else {
-    indexMyFavorite.style.display = "block";
-  }
-}
